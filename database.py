07163f97bfc0e620b8085797651bebd0f579ed51
ealtunel <demirhan.altunel+github@gmail.com>
2023-10-26 01:28:05 +0300

    Added req file and add some comments

--- database.py
+++ database.py
@@ -3,53 +3,176 @@ import threading
 import bcrypt
 from user import User
 from typing import Optional
+import logging
+import re
+from typing import Any
+
 
 class Database:
+	"""
+	A class representing a SQLite database.
+
+	Attributes:
+	- database_name (str): The name of the database.
+	- connection (sqlite3.Connection): The connection object for the database.
+	- lock (threading.Lock): A lock object for thread safety.
+
+	Methods:
+	- __init__(self, database_name: str) -> None: Initializes the Database object.
+	- create_tables(self) -> None: Creates the necessary tables in the database.
+	- execute_query(self, query: str, params: tuple = None) -> list: Executes a query on the database.
+	- insert(self, table: str, values: dict) -> None: Inserts a row into the specified table.
+	- select(self, table: str, columns: list = None, where: str = None, params: tuple = None) -> list: Selects rows from the specified table.
+	- update(self, table: str, values: dict, where: str = None, params: tuple = None) -> None: Updates rows in the specified table.
+	- delete(self, table: str, where: str = None, params: tuple = None) -> None: Deletes rows from the specified table.
+	"""
+
 	def __init__(self, database_name: str) -> None:
-		self.conection = sqlite3.connect(database_name)
-		self.lock = threading.Lock()
-		self.create_tables()
-	
+		"""
+		Initializes the Database object.
+
+		Args:
+		- database_name (str): The name of the database.
+
+		Returns:
+		- None
+		"""
+		try:
+			logging.info(f"Connecting to database {database_name}")
+			self.connection = sqlite3.connect(database_name)
+			logging.info(f"Connected to database {database_name}")
+			self.lock = threading.Lock()
+			self.create_tables()
+		except Exception as e:
+			logging.error(e)
+			self.connection = None
+
 	def __del__(self):
 		self.conection.close()
-	
-	def create_tables(self):
+
+	@staticmethod
+	def check_values_not_none(*args: Any) -> bool:
+		"""
+		Checks if all given values are not None.
+
+		Args:
+		- *args (Any): The values to check.
+
+		Returns:
+		- bool: True if all values are not None, False otherwise.
+		"""
+		for arg in args:
+			if arg is None:
+				return False
+		return True
+
+	def create_tables(self) -> None:
+		"""
+		Creates the necessary tables in the database.
+
+		Args:
+		- None
+
+		Returns:
+		- None
+		"""
+		logging.info("Creating tables")
+		if self.conection is None:
+			logging.error("Database not connected")
+			return
 		self.lock.acquire()
 		cursor = self.conection.cursor()
-		cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-				 user_id INTEGER PRIMARY KEY AUTOINCREMENT, 
-				 name TEXT, 
-				 email TEXT, 
-				 password BLOB,
-				 authorization INTEGER,
-				 UNIQUE(name, email))""")
-		cursor.execute("""CREATE TABLE IF NOT EXISTS images (
-				 image_id INTEGER PRIMARY KEY AUTOINCREMENT, 
-				 user_id INTEGER,
-				 image_data BLOB)""")
+		cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS users (
+				user_id			INTEGER PRIMARY KEY AUTOINCREMENT,
+				username		TEXT,
+				email			TEXT,
+				password		BLOB,
+				authorization	INTEGER,
+				UNIQUE(name, email))
+				"""
+		)
+		cursor.execute(
+			"""CREATE TABLE IF NOT EXISTS images (
+				image_id	INTEGER PRIMARY KEY AUTOINCREMENT, 
+				user_id		INTEGER,
+				image_data	BLOB)
+				"""
+		)
 		self.conection.commit()
 		self.lock.release()
-	
-	def insert_user(self, name: str, email: str, password: str, authorization: int) -> Optional[int]:
+
+	def insert_user(
+		self, username: str, email: str, password: str, authorization: int
+	) -> Optional[int]:
+		"""
+		Inserts a new user into the database.
+
+		Args:
+		- username (str): The username of the user.
+		- email (str): The email of the user.
+		- password (str): The password of the user.
+		- authorization (int): The authorization level of the user.
+
+		Returns:
+		- Optional[int]: The ID of the user, or None if the user was not inserted or if one or more values are None.
+		"""
+		logging.info(f"Inserting new user with name {username}")
+		if not self.check_values_not_none(username, email, password, authorization):
+			logging.error("One or more values are None")
+			return None
+		try:
+			User.check_valid_user(username, email, password, authorization)
+		except ValueError as e:
+			logging.error(e)
+			return None
 		self.lock.acquire()
 		cursor = self.conection.cursor()
-		return_value: Optional[int]= None
+		user_id: Optional[int] = None
 		try:
-			cursor.execute("""INSERT INTO users VALUES (NULL, ?, ?, ?, ?)""", 
-				 (name, email, bcrypt.hashpw(password.encode(), bcrypt.gensalt()), authorization))
-			return_value = cursor.lastrowid
+			cursor.execute(
+				"""INSERT INTO users VALUES (NULL, ?, ?, ?, ?)""",
+				(
+					username,
+					email,
+					bcrypt.hashpw(password.encode(), bcrypt.gensalt()),
+					authorization,
+				),
+			)
+			user_id = cursor.lastrowid
 			self.conection.commit()
+			logging.info(f"{username} inserted successfully with id {user_id}")
 		except Exception as e:
-			return_value = None
+			logging.error(e)
+			user_id = None
 		finally:
 			self.lock.release()
-			return return_value
-	
-	def get_user(self, username:str, password:str) -> Optional[User]:
+			return user_id
+
+	def get_user(self, username: str, password: str) -> Optional[User]:
+		"""
+		Retrieves a user from the database with the given username and password.
+
+		Args:
+		- username (str): The username of the user.
+		- password (str): The password of the user.
+
+		Returns:
+		- Optional[User]: The user, or None if the user was not found or if one or more values are None.
+		"""
+		logging.info(f"Getting user with name {username}")
+		if not self.check_values_not_none(username, password):
+			logging.error("One or more values are None")
+			return None
+		try:
+			User.check_valid_name(username)
+			User.check_valid_password(password)
+		except ValueError as e:
+			logging.error(e)
+			return None
 		self.lock.acquire()
 		cursor = self.conection.cursor()
-		cursor.execute("""SELECT * FROM users WHERE name=?""", 
-				 (username,))
+		cursor.execute("""SELECT * FROM users WHERE name=?""", (username,))
 		row: tuple = cursor.fetchone()
 		if row is None:
 			self.lock.release()
@@ -61,26 +184,73 @@ class Database:
 		self.lock.release()
 		return user
 
-	def insert_image(self, user_id: int, image) -> Optional[int]:
+	def insert_image(self, user_id: int, image: bytearray) -> Optional[int]:
+		logging.info(f"Inserting image for user with id {user_id}")
+		if not self.check_values_not_none(user_id, image):
+			logging.error("One or more values are None")
+			return None
 		self.lock.acquire()
 		cursor = self.conection.cursor()
-		return_value: Optional[int] = None
+		image_id: Optional[int] = None
 		try:
-			cursor.execute("""INSERT INTO images VALUES (NULL, ?, ?)""", 
-				 (user_id, image))
-			return_value = cursor.lastrowid
+			cursor.execute(
+				"""INSERT INTO images VALUES (NULL, ?, ?)""", (user_id, image)
+			)
+			image_id = cursor.lastrowid
 			self.conection.commit()
+			logging.info(f"Image inserted successfully with id {image_id}")
 		except Exception as e:
-			return_value = None
+			logging.error(e)
+			image_id = None
 		finally:
 			self.lock.release()
-			return return_value
-		
-	def get_image(self, image_id:int) -> Optional[User]:
-		self.lock.acquire()
-		cursor = self.conection.cursor()
-		cursor.execute("""SELECT * FROM images WHERE image_id=?""", 
-				 (image_id,))
-		row: tuple = cursor.fetchone()
-		self.lock.release()
-		return row
+			return image_id
+
+	class Database:
+		def __init__(self, db_path: str):
+			"""
+			Initializes a new instance of the Database class.
+
+			Args:
+					db_path (str): The path to the SQLite database file.
+			"""
+			self.db_path = db_path
+			self.conection = sqlite3.connect(db_path)
+			self.lock = threading.Lock()
+
+		def check_values_not_none(self, *values: Any) -> bool:
+			"""
+			Checks if all given values are not None.
+
+			Args:
+					*values (Any): The values to check.
+
+			Returns:
+					bool: True if all values are not None, False otherwise.
+			"""
+			return all(value is not None for value in values)
+
+		def get_image(self, image_id: int) -> Optional[bytearray]:
+			"""
+			Retrieves an image from the database with the given image_id.
+
+			Args:
+					image_id (int): The ID of the image to retrieve.
+
+			Returns:
+					Optional[bytearray]: The image data as a bytearray, or None if the image was not found or if one or more values are None.
+			"""
+			logging.info(f"Getting image with id {image_id}")
+			if not self.check_values_not_none(image_id):
+				logging.error("One or more values are None")
+				return None
+			self.lock.acquire()
+			cursor = self.conection.cursor()
+			cursor.execute(
+				"""SELECT * FROM images WHERE image_id=?""", (image_id,))
+			row: tuple = cursor.fetchone()
+			if row is None:
+				self.lock.release()
+				return None
+			self.lock.release()
+			return row[2]
--- user.py
+++ user.py
@@ -1,9 +1,110 @@
+import re
+
+
 class User:
+	"""
+	A class representing a user.
+
+	Attributes:
+	- id (int): The user's ID.
+	- name (str): The user's name.
+	- email (str): The user's email.
+	- authorization (int): The user's authorization level.
+
+	Methods:
+	- __init__(self, id: int, name: str, email: str, authorization: int): Constructs a new User object.
+	- __str__(self) -> str: Returns a string representation of the User object.
+	- check_valid_user(name: str, email: str, password: str, authorization: int) -> bool: Checks if a user is valid.
+	- check_valid_name(name: str) -> bool: Checks if a name is valid.
+	- check_valid_email(email: str) -> bool: Checks if an email is valid.
+	- check_valid_password(password: str) -> bool: Checks if a password is valid.
+	- check_valid_authorization(authorization: int) -> bool: Checks if an authorization level is valid.
+	"""
+
 	def __init__(self, id: int, name: str, email: str, authorization: int):
+		"""
+		Constructs a new User object.
+
+		Args:
+		- id (int): The user's ID.
+		- name (str): The user's name.
+		- email (str): The user's email.
+		- authorization (int): The user's authorization level.
+		"""
 		self.id = id
 		self.name = name
 		self.email = email
 		self.authorization = authorization
-	
+
 	def __str__(self) -> str:
+		"""
+		Returns a string representation of the User object.
+		
+		Returns:
+		- str: A string representation of the User object.
+		"""
 		return f"User: {self.name}, {self.email}, {self.authorization}"
+
+	@staticmethod
+	def check_valid_user(
+			name: str, email: str, password: str, authorization: int
+	) -> bool:
+		"""
+		Checks if a user is valid.
+					
+		Args:
+		- name (str): The name of the user.
+		- email (str): The email of the user.
+		- password (str): The password of the user.
+		- authorization (int): The authorization level of the user.
+					
+		Returns:
+		- bool: True if the user is valid, False otherwise.
+		"""
+		return (
+			User.check_valid_name(name)
+			and User.check_valid_email(email)
+			and User.check_valid_password(password)
+			and User.check_valid_authorization(authorization)
+		)
+
+	@staticmethod
+	def check_valid_name(name: str) -> bool:
+		if len(name) > 32 or len(name) < 6:
+			raise ValueError("Name must be between 6 and 32 characters")
+		if re.match(r"[a-zA-Z][a-zA-Z0-9\._]+", name) is None:
+			raise ValueError(
+				"Name must start with a letter and contain only alphanumeric characters, underscores, and periods"
+			)
+		return True
+
+	@staticmethod
+	def check_valid_email(email: str) -> bool:
+		if len(email) > 64 or len(email) < 6:
+			raise ValueError("Email must be between 6 and 64 characters")
+		if re.match(r"[^@]+@[^@]+\.[^@]+", email) is None:
+			raise ValueError("Email must be a valid email address")
+		return True
+
+	@staticmethod
+	def check_valid_password(password: str) -> bool:
+		if len(password) < 8 or len(password) > 32:
+			raise ValueError("Password must be between 8 and 32 characters")
+		if re.search("[a-z]", password) is None:
+			raise ValueError(
+				"Password must contain at least one lowercase letter")
+		if re.search("[A-Z]", password) is None:
+			raise ValueError(
+				"Password must contain at least one uppercase letter")
+		if re.search("[0-9]", password) is None:
+			raise ValueError("Password must contain at least one number")
+		if not password.isalnum():
+			raise ValueError(
+				"Password must contain only alphanumeric characters")
+		return True
+
+	@staticmethod
+	def check_valid_authorization(authorization: int) -> bool:
+		if authorization < 0 or authorization > 2:
+			raise ValueError("Authorization must be between 0 and 2")
+		return True
