e419f92273cbb3abf10ff56c58aeb1f4128bd571
e-altunel <demirhan.altunel+github@gmail.com>
2023-12-11 15:13:46 +0300

    django removed

--- BeslemeKahramanlari/PostApp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-from django.contrib.auth.models import User
-
-
-class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ["title", "food_point", "content", "image"]
