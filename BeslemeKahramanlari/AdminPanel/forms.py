e7f43baf615d1322acb4b67dda32dded847dffcb
e-altunel <demirhan.altunel+github@gmail.com>
2023-12-11 16:21:22 +0300

    add django

---
+++ BeslemeKahramanlari/AdminPanel/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from MobileAPI.models import *
+
+class FeedPointForm(forms.ModelForm):
+	class Meta:
+		model = FeedPoint
+		field = ['name', 'latitude', 'longitude', 'food_amount']
+		exclude = ['id', 'created_at', 'updated_at']
+		widgets = {
+			'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Besleme Noktası Adı'}),
+			'latitude': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enlem'}),
+			'longitude': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Boylam'}),
+			'food_amount': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Yem Miktarı'}),
+		}
---
+++ BeslemeKahramanlari/BeslemeKahramanlari/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+   	path('api/', include('MobileAPI.urls')),
+   	path('panel/', include('AdminPanel.urls')),
+   	path('accounts/', include('AdminPanel.urls')),
+]
