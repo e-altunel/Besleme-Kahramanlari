d3e5ec884babdd5c9244115c5d9e170de5a9ccad
e-altunel <demirhan.altunel+github@gmail.com>
2024-01-07 20:12:49 +0300

    Add food level

--- BeslemeKahramanlari/AdminPanel/forms.py
+++ BeslemeKahramanlari/AdminPanel/forms.py
@@ -1,21 +1,55 @@
 from django import forms
 from MobileAPI.models import *
 
+
 class FeedPointForm(forms.ModelForm):
-	class Meta:
-		model = FeedPoint
-		field = ['name', 'latitude', 'longitude', 'food_amount']
-		exclude = ['id', 'created_at', 'updated_at']
-		widgets = {
-			'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Besleme Noktası Adı'}),
-			'latitude': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enlem'}),
-			'longitude': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Boylam'}),
-			'food_amount': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Yem Miktarı'}),
-		}
+    def __init__(self, *args, **kwargs):
+        super(FeedPointForm, self).__init__(*args, **kwargs)
+        self.instance.calculate_food_level()
+
+    class Meta:
+        model = FeedPoint
+        field = [
+            "name",
+            "latitude",
+            "longitude",
+        ]
+        exclude = [
+            "id",
+            "created_at",
+            "updated_at",
+            "is_active",
+            "food_amount",
+            "food_avarage_amount",
+            "last_calculated_at",
+            "should_update",
+        ]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Besleme Noktası Adı"}
+            ),
+            "latitude": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enlem"}
+            ),
+            "longitude": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Boylam"}
+            ),
+            "food_level": forms.TextInput(
+                attrs={"readonly": "readonly", "class": "form-control"}
+            ),
+        }
 
 
 class LoginForm(forms.Form):
-	username = forms.CharField(max_length=100, widget=forms.TextInput(
-		attrs={'class': 'form-control', 'placeholder': 'Kullanıcı Adı'}))
-	password = forms.CharField(max_length=100, widget=forms.PasswordInput(
-		attrs={'class': 'form-control', 'placeholder': 'Şifre'}))
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Kullanıcı Adı"}
+        ),
+    )
+    password = forms.CharField(
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Şifre"}
+        ),
+    )
